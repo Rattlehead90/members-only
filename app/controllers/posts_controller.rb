4c4fdad7016c61f7bb80aee4b6f43c2049224c7e
Rattlehead90 <strogonov@seznam.cz>
2022-11-13 21:37:34 +0100

    Set up posting mechanism for logged in users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,13 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(title: params[:post][:title], content: params[:post][:content],
+                     user_id: current_user.id)
 
+    if @post.save
+      redirect_to new_post_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
